4ee526c48d2a6d2c254d9b36e93de0a355620d2a
Jorge <jorgeluizkoch@gmail.com>
2026-08-06 15:21:05 -0300

    analytics added

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,17 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
+
 import App from "./App";
 import { GlobalStyle } from "./styles/GlobalStyle";
 import { ThemeModeProvider } from "./contexts/ThemeModeContext";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <ThemeModeProvider>
         <GlobalStyle />
         <App />
       </ThemeModeProvider>
+
+      <Analytics />
     </BrowserRouter>
   </React.StrictMode>
 );
