603db8759d9fc4c7dae9406e3e6d4538a1035940
Jorge Koch <jorgeluizkoch@gmail.com>
2026-08-12 15:30:02 -0300

    fix(auth): use shared admin logout helper in user menu

--- frontend/src/components/admin/UserMenu/index.tsx
+++ frontend/src/components/admin/UserMenu/index.tsx
@@ -5,72 +5,45 @@ import {
   User,
 } from "lucide-react";
 
+import { removeAdminToken } from "../../../lib/auth";
+
 import * as S from "./styles";
 
 export function UserMenu() {
   function handleLogout() {
-    localStorage.removeItem("room.token");
-
+    removeAdminToken();
     window.location.href = "/admin/login";
   }
 
   return (
     <S.Container>
-
       <S.Trigger>
-
-        <S.Avatar>
-          JK
-        </S.Avatar>
-
+        <S.Avatar>JK</S.Avatar>
         <S.Info>
-
-          <strong>
-            Jorge Koch
-          </strong>
-
-          <span>
-            OWNER
-          </span>
-
+          <strong>Jorge Koch</strong>
+          <span>OWNER</span>
         </S.Info>
-
         <ChevronDown size={18} />
-
       </S.Trigger>
 
       <S.Dropdown>
-
         <S.Item>
-
           <User size={16} />
-
           Meu Perfil
-
         </S.Item>
 
         <S.Item>
-
           <Settings size={16} />
-
           Configurações
-
         </S.Item>
 
         <S.Divider />
 
-        <S.Item
-          onClick={handleLogout}
-        >
-
+        <S.Item onClick={handleLogout}>
           <LogOut size={16} />
-
           Sair
-
         </S.Item>
-
       </S.Dropdown>
-
     </S.Container>
   );
 }
